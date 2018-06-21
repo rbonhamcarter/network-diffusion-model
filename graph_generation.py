06f09f46566b22354ee4dc8ac839852f4fbbee3b
rebecca bonham-carter <rbonhamc@eubee.inria.fr>
2018-06-21 18:09:27 +0200

    changes made to give meaning to position and edge colour in graph via plot_connectome in nilearn

--- graph_generation.py
+++ graph_generation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
+from nilearn import plotting
 from os.path import expanduser, join
 
 
@@ -11,14 +12,29 @@ def main():
     data_file_name = join(directory_name, 'func_modeled.npy')
     struct_conn = np.load(data_file_name)
 
-    # Generate graph
-    G = nx.from_numpy_array(struct_conn)
+    # # Generate graph
+    # G = nx.from_numpy_array(struct_conn)
+    #
+    # # Visualize graph - no meaning to position or colour
+    # plt.figure('Graph')
+    # n_node = G.number_of_nodes()  # number of nodes
+    # nx.draw(G, node_size=50, node_color=np.arange(n_node),  width=0.1, cmap='Blues')
+    # plt.show()
 
-    # Visualize graph
-    plt.figure('Graph')
-    print(G.number_of_edges())
-    nx.draw(G, node_size=50, node_color=np.arange(144),  width=0.1, cmap='Blues')
-    plt.show()
+    # Visualise graph with positions specified by brain region coordinates
+    # # Import node positions from file of (x,y,z) co-ordinates from brain region centers
+    positions_file_name = join(directory_name, 'region_positions.npy')
+    region_positions = np.load(positions_file_name)
+    plotting.plot_connectome(struct_conn,
+                             region_positions - region_positions.mean(0),
+                             title="Structural Connectivity",
+                             display_mode='x',
+                             alpha=0,
+                             node_color='k',
+                             node_size=10,
+                             edge_cmap='jet',
+                             colorbar=True)
+    plotting.show()
 
 
 main()
